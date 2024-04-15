f5c0d9e0a8cfaca7512fa9944f5454436c324512
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-04-15 18:51:26 +0200

    the simplest webview example

---
+++ javafx-example-4/src/main/java/com/cthiebaud/javafx/HelloJavaFXWebViewWorld.java
@@ -0,0 +1,45 @@
+package com.cthiebaud.javafx;
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+/**
+ * The main application class.
+ */
+public class HelloJavaFXWebViewWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Set the title of the stage
+        primaryStage.setTitle(this.getClass().getSimpleName());
+
+        // Create a WebView
+        WebView webView = new WebView();
+
+        // Load a webpage
+        webView.getEngine().load("https://aequologica.net/");
+
+        // Retrieve primary screen information
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        // Create a Scene with WebView as the root node
+        Scene scene = new Scene(webView, bounds.getWidth(), bounds.getHeight());
+
+        // Set the scene to the stage
+        primaryStage.setScene(scene);
+
+        // Show the stage
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ javafx-example-4/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.cthiebaud.javafx {
+    requires javafx.controls;
+    requires transitive javafx.web;
+
+    exports com.cthiebaud.javafx;
+}
