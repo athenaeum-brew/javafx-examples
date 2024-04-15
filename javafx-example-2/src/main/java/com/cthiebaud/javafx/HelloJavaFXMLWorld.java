167da9ee7f6ae1b5ae29dfd5996b324da50301ab
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-04-15 17:45:05 +0200

    debug + title

--- javafx-example-2/src/main/java/com/cthiebaud/javafx/HelloJavaFXMLWorld.java
+++ javafx-example-2/src/main/java/com/cthiebaud/javafx/HelloJavaFXMLWorld.java
@@ -17,6 +17,7 @@ public class HelloJavaFXMLWorld extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle(this.getClass().getSimpleName());
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
